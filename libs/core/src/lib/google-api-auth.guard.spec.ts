40d263f6afb5e1a894c72e87b11a5fef3c774aba
sojuz <adam.mielko@instapage.com>
2018-08-03 13:56:46 +0200

    ng g g google-api-auth --project=core

---
+++ libs/core/src/lib/google-api-auth.guard.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed, async, inject } from '@angular/core/testing';
+
+import { GoogleApiAuthGuard } from './google-api-auth.guard';
+
+describe('GoogleApiAuthGuard', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [GoogleApiAuthGuard]
+    });
+  });
+
+  it('should ...', inject([GoogleApiAuthGuard], (guard: GoogleApiAuthGuard) => {
+    expect(guard).toBeTruthy();
+  }));
+});
